bc04e10f09549c97ecd34456ad69c13229ad87f1
BelyakovKamil <kamil.belyakov@mail.ru>
2023-01-14 12:02:09 +0300

    Creatted first view

--- FirstDjangoProject/mysite/mysite/settings.py
+++ FirstDjangoProject/mysite/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-(48^01+#zngf8z@bot)(9ordu2y5=m(q8pp++!@1zyn2yp60h+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.92.100']
+ALLOWED_HOSTS = ['192.168.92.100', 'localhost', '127.0.0.1']
 
 
 # Application definition
